9e5638d6d2728f61f8e28b8adda9fedee24f7f9a
Kusuma-codes <kusumakushi960@gmail.com>
2025-07-11 19:00:14 +0530

    Added new program

---
+++ Prime_Number.py
@@ -0,0 +1,13 @@
+def prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+number = 15
+if prime(number):
+    print(f"{number} is Prime Number.")
+else:
+    print(f"{number} is not a prime number")
