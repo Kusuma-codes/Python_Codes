ace85d24b375a82c5d969b4e61d4018f05619b9b
Kusuma-codes <kusumakushi960@gmail.com>
2025-07-15 20:05:40 +0530

    Program added

---
+++ Fibonacci_Series.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+    a, b = 0, 1
+    print("Fibonacci Series:")
+    for _ in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+
+terms = int(input("Enter how many terms you want: "))
+fibonacci(terms)
