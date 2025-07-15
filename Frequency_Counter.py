15480f47130f2991bac659f4266260ac32fa01ec
Kusuma <kusumakushi960@gmail.com>
2025-07-15 19:52:10 +0530

    Delete Frequency_Counter.py

--- Frequency_Counter.py
+++
@@ -1,7 +0,0 @@
-num1 = input("Enter Numbers: ")
-num = list(map(int, num1.split()))
-frequency = {f:0 for f in num}
-for i in num:
-    frequency[i] += 1
-for f in sorted(frequency):
-    print(f"{f} = {frequency[f]}")
