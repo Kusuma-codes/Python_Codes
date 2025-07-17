b01af72c4d3b52f063d7ec9041498ef72b6f52e9
Kusuma-codes <kusumakushi960@gmail.com>
2025-07-17 19:12:39 +0530

    Added program

--- Fibonacci_Series.py
+++ Fibonacci_Series.py
@@ -1,7 +1,7 @@
 def fibonacci(n):
     a, b = 0, 1
     print("Fibonacci Series:")
-    for _ in range(n):
+    for i in range(n):
         print(a, end=" ")
         a, b = b, a + b
 
---
+++ Number_pyramid.py
@@ -0,0 +1,9 @@
+a = 1
+n = 6
+
+for i in range(1, n):
+    for j in range(1, i+1):
+        print(a, end= " ")
+        a += 1
+    print()
+    
