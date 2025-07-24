242c70bf29069882ec1b308bd93d85700bdd25c5
Kusuma-codes <kusumakushi960@gmail.com>
2025-07-24 21:41:21 +0530

    Set program

---
+++ Set_program.py
@@ -0,0 +1,8 @@
+# Define a list with duplicates
+num_list = [10, 20, 10, 30, 20, 40, 50]
+
+# Convert list to set to remove duplicates
+unique_numbers = set(num_list)
+
+# Display result
+print("Unique numbers:", unique_numbers)
