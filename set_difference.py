028dd7f0cb8c77890b9db4656c9c29a8f5d75fd7
Kusuma-codes <kusumakushi960@gmail.com>
2025-07-25 19:33:38 +0530

    Added program

---
+++ set_difference.py
@@ -0,0 +1,9 @@
+# Define two sets
+set1 = {100, 200, 300, 400}
+set2 = {300, 400, 500}
+
+# Find elements only in set1
+only_in_set1 = set1.difference(set2)
+
+# Display result
+print("Elements only in set1:", only_in_set1)
