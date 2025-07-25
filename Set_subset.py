1cab8dc8b1a5ea2a803299d371dafd239a9084e6
Kusuma-codes <kusumakushi960@gmail.com>
2025-07-25 19:35:42 +0530

    Added program

---
+++ Set_subset.py
@@ -0,0 +1,9 @@
+# Define two sets
+setA = {1, 2, 3}
+setB = {1, 2, 3, 4, 5}
+
+# Check if setA is a subset of setB
+if setA.issubset(setB):
+    print("setA is a subset of setB")
+else:
+    print("setA is not a subset of setB")
