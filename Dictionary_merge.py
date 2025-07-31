805eb166f5fa603bc3061e7ed18c89bdc0fb68e1
Kusuma-codes <kusumakushi960@gmail.com>
2025-07-31 18:32:59 +0530

    Added program

---
+++ Dictionary_merge.py
@@ -0,0 +1,8 @@
+dict1 = {'a': 10, 'b': 20}
+dict2 = {'c': 30, 'd': 40}
+
+# Method 1: Using unpacking operator **
+merged_dict = {**dict1, **dict2}
+
+# Print merged dictionary
+print("Merged Dictionary:", merged_dict)
