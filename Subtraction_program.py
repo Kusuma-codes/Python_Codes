fd92e778498e59b66d4e0c7d7c1e1699acba4426
Kusuma-codes <kusumakushi960@gmail.com>
2025-08-06 22:52:24 +0530

    Added program

---
+++ Subtraction_program.py
@@ -0,0 +1,9 @@
+def subtract(a, b):
+    result = a - b
+    return result
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+difference = subtract(num1, num2)
+print("Subtraction Result:", difference)
