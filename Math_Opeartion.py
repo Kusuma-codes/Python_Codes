e3144e13a79f130c5491e9ce80a57b81110865fc
Kusuma-codes <kusumakushi960@gmail.com>
2025-08-14 18:37:40 +0530

    Added program

---
+++ Math_Opeartion.py
@@ -0,0 +1,14 @@
+#Math Opeartion
+import math
+
+num = float(input("Enter a number: "))
+
+print("\nMath Operations using math library:")
+print("Square root:", math.sqrt(num))
+print("Power (num^2):", math.pow(num, 2))
+print("Ceiling value:", math.ceil(num))
+print("Floor value:", math.floor(num))
+print("Factorial (only works for integers):", math.factorial(int(num)))
+print("Sine of number (in radians):", math.sin(num))
+print("Cosine of number (in radians):", math.cos(num))
+print("Logarithm base e:", math.log(num))
