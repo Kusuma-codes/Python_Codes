d28537bbded1a559575d9475d20bf785b04d2ad6
Kusuma-codes <kusumakushi960@gmail.com>
2025-08-16 20:04:22 +0530

    Added program

---
+++ Relational_Operators.py
@@ -0,0 +1,13 @@
+# Relational (Comparison) Operators Program
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("\nRelational Operators Demonstration:")
+
+print(f"{a} == {b} : {a == b}")   
+print(f"{a} != {b} : {a != b}")   
+print(f"{a} >  {b} : {a > b}")    
+print(f"{a} <  {b} : {a < b}")    
+print(f"{a} >= {b} : {a >= b}")   
+print(f"{a} <= {b} : {a <= b}")   
