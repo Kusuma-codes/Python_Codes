2d970055ed91cc14fee12a0c37324e0f7a8b5965
Kusuma-codes <kusumakushi960@gmail.com>
2025-08-23 18:51:34 +0530

    Added program

---
+++ List_slice.py
@@ -0,0 +1,15 @@
+# Python Program to Slice Lists
+
+numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+
+print("Original List:", numbers)
+
+print("Sliced list [2:6]:", numbers[2:6])
+
+print("Sliced list [:5]:", numbers[:5])
+
+print("Sliced list [3:]:", numbers[3:])
+
+print("Sliced list [::2]:", numbers[::2])
+
+print("Reversed List:", numbers[::-1])
