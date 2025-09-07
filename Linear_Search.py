361ffab7a70be60a20a6dab8c30f171a33c202be
Kusuma-codes <kusumakushi960@gmail.com>
2025-09-07 21:02:34 +0530

    Added program

---
+++ Linear_Search.py
@@ -0,0 +1,16 @@
+#Linear Search
+def linear_search(arr, x):
+    for i in range(len(arr)):
+        if arr[i] == x:
+            return i
+    return -1
+
+arr = [4, 2, 7, 1, 9, 5]
+x = 7
+
+result = linear_search(arr, x)
+
+if result != -1:
+    print(f"Element {x} found at index {result}")
+else:
+    print(f"Element {x} not found")
