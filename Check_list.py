67450d52979c8ca4988e0c19c4c1a0023e377d8c
Kusuma <kusumakushi960@gmail.com>
2025-09-09 22:04:12 +0530

    Delete Check_list.py

--- Check_list.py
+++
@@ -1,7 +0,0 @@
-my_list = [10, 20, 30, 40, 50]
-element = int(input("Enter the element to check: "))
-
-if element in my_list:
-    print(f"{element} is present in the list.")
-else:
-    print(f"{element} is not present in the list.")
